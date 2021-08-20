d4f58ca1796133806e18e0b38fbf5043d4fee8c2
Rif Mo <piuuuuuuui@outlook.com>
2021-08-20 14:11:24 +0800

    增加 board, grids 和 pieces 。

--- chess.cpp
+++ chess.cpp
@@ -1,10 +1,33 @@
 #include <qstyle.h>
 #include "chess.h"
-#include "chessboard.h"
 
 Chess::Chess(QWidget *parent) : QMainWindow(parent) {
   ui.setupUi(this);
   setFixedSize(width(), height());
-  Chessboard *board = new Chessboard(centralWidget());
-  board->show();
+  setBoard();
+  setPieces();
+}
+
+void Chess::setBoard() {
+  board_ = new QWidget(centralWidget());
+  board_->setGeometry(0, 0, 441, 614);
+  board_->setStyleSheet("border-image:url(:/Chess/Grids)");
+  for (int i = 0; i < 6; i++)
+    for (int j = 0; j < 5; j++)
+      grids_.push_back(QRect(j * 94, i * 44, 65, 31));
+  for (int i = 0; i < 6; i++)
+    for (int j = 0; j < 5; j++)
+      grids_.push_back(QRect(j * 94, i * 44 + 360, 65, 31));
+  board_->show();
+}
+
+void Chess::setPieces() {
+  for (int i = 0; i < 60; i++) {
+    pieces_.push_back(new QWidget(centralWidget()));
+    if (true) {
+      pieces_[i]->setGeometry(grids_[i]);
+      pieces_[i]->setStyleSheet("border-image:url(:/Chess/UN)");
+      pieces_[i]->show();
+    }
+  }
 }
--- chess.h
+++ chess.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <QtWidgets/QMainWindow>
+#include <QList>
 #include "ui_chess.h"
 
 class Chess : public QMainWindow {
@@ -8,4 +9,9 @@ public:
   Chess(QWidget *parent = Q_NULLPTR);
 private:
   Ui::ChessClass ui;
+  QWidget *board_;
+  QList<QRect> grids_;
+  QList<QWidget *> pieces_;
+  void setBoard();
+  void setPieces();
 };
