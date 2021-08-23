02284c39eb95f1b28acd74f8b9883bb59a7ea36f
Rif Mo <piuuuuuuui@outlook.com>
2021-08-23 21:22:18 +0800

    增加 Connect 菜单功能。(bug)

--- chess.cpp
+++ chess.cpp
@@ -4,18 +4,47 @@ Chess::Chess(QWidget *parent) : QMainWindow(parent) {
   ui.setupUi(this);
   setFixedSize(width(), height());
 
-  game = new Game(centralWidget());
-  game->setGeometry(0, -100, 441, 614);
-
-  connect(ui.actionStart, SIGNAL(triggered(bool)), this, SLOT(gameStart()));
-  connect(ui.actionAdmit_defeat, SIGNAL(triggered(bool)), game, SLOT(lose()));
-  connect(game, SIGNAL(over()), this, SLOT(gameOver()));
-  connect(game, SIGNAL(enableSurrender(bool)), ui.actionAdmit_defeat, SLOT(setEnabled(bool)));
+  connect(ui.actionCreate_the_connection, &QAction::triggered, this, &Chess::setServer);
+  connect(ui.actionConnect_to_server, &QAction::triggered, this, &Chess::setClient);
+  connect(ui.actionDisconnect, &QAction::triggered, this, &Chess::disconnect);
+  connect(ui.actionStart, &QAction::triggered, this, &Chess::gameStart);
 }
 
 void Chess::paintEvent(QPaintEvent *event) {
   QPainter p(this);
   p.drawPixmap(QRect(0, 21, 441, 614), QPixmap(":/Chess/Background"));
+}
+
+void Chess::connecting(bool x) {
+  ui.actionCreate_the_connection->setEnabled(!x);
+  ui.actionConnect_to_server->setEnabled(!x);
+  ui.actionDisconnect->setEnabled(x);
+  ui.actionStart->setEnabled(x);
+}
+
+void Chess::gameInit() {
+  game = new Game(centralWidget());
+  game->setGeometry(0, -100, 441, 614);
+  game->show();
+  connect(ui.actionAdmit_defeat, &QAction::triggered, game, &Game::lose);
+  connect(game, &Game::over, this, &Chess::gameOver);
+  connect(game, &Game::enableSurrender, ui.actionAdmit_defeat, &QAction::setEnabled);
+}
+
+void Chess::setServer() {
+  server = new QTcpServer();
+  qDebug() << server->listen();
+  connecting(true);
+  gameInit();
+}
+
+void Chess::setClient() {
+  connecting(true);
+}
+
+void Chess::disconnect() {
+  delete game;
+  connecting(false);
 }
 
 void Chess::gameStart() {
